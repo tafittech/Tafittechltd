04468b855a7af0be07d158ba8b4c38eb39e5a3b1
Trevor Jemmott <tafittechtt@gmail.com>
2022-10-03 10:43:34 -0400

    second committ cart models fixed

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,36 @@
+from django.conf import settings
 from django.db import models
 
+
+#imports from app modules.
+
+from products.models import Product
+
+#create model manager here 
+class CartModelManger(models.Manager):
+    def new_cart(self, user=None):
+        user_obj =None
+        if user is not None:
+            if user.is_authenticated():
+                user_obj = user_obj
+        return self.model.objects.create(user=user_obj)
+
+
+User = settings.AUTH_USER_MODEL
 # Create your models here.
+class Carts(models.Model):
+    user       = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    products   = models.ManyToManyField(Product, blank=True)
+    subtotal   = models.DecimalField(default=0.00, decimal_places=2, max_digits=9999)
+    total      = models.DecimalField(default=0.00, decimal_places=2, max_digits=9999)
+    update     = models.TimeField(auto_now=True)
+    timestamp  = models.TimeField(auto_now_add=True)
+
+
+    objects = CartModelManger()
+
+
+
+
+    def __str__(self):
+        return str(self.id)
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
 
+
+
+#imports from apps here.
+from .models import Carts
+
+
+
+
 # Create your views here.
 def cart_home_page(request):
+    cart_id = request.session.get('cart_id', None)
+    qs = Carts.objects.filter(id=cart_id)
+    if cart_id is None:
+        if qs.count() == 1:
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Carts.objects.new_cart(user=request.user)
+            request.session['cart_id']=cart_obj.id
     return render(request, 'cart/cart-home.html', {})
 
 
